b999439ff8861728c9a5415f6b94d76f6b41d9cc
lioshi <lionel.fenneteau@gmail.com>
2017-07-21 14:17:18 +0200

    fixed: preg_replace

--- lib/sfWebBrowser.class.php
+++ lib/sfWebBrowser.class.php
@@ -852,7 +852,8 @@ class sfWebBrowser
    */
   protected function normalizeHeaderName($name)
   {
-    return preg_replace('/\-(.)/e', "'-'.strtoupper('\\1')", strtr(ucfirst($name), '_', '-'));
+    return preg_replace_callback('/\-(.)/', function($m) { return '-'.strtoupper($m[1]); }, strtr(ucfirst($name), '_', '-'));
+    // return preg_replace('/\-(.)/e', "'-'.strtoupper('\\1')", strtr(ucfirst($name), '_', '-'));
   }
 
 }
